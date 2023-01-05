567b2cb68cac508669550f63dff1285ae3ccc0a8
RandeeScows <carppooli3yuan1@gmail.com>
2023-01-05 22:25:18 +0900

    post.jp adjustment 1

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -2,7 +2,7 @@
 function post(){
   const submit = document.getElementById("submit_btn");
   submit.addEventlistener("click", (e)=> {
-    const formData = new.FormData(document.getElementById("new_article"));
+    const formData = new FormData(document.getElementById("new_article"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST","/articles", true);
     XHR.responseType ="json";
